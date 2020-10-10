04da43a5cd2dd06633c5055db2b92d6022cb2bc1
Omar Ali <omarali0601520286@gmail.com>
2020-10-10 21:55:55 +0100

    database access integration

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'miniinscription'
+})
 
+connection.connect()
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  connection.query('SELECT count(*) AS couterAll from member', function (err, rows, fields) {
+    if (err) throw err
+  
+    console.log('The solution is: ', rows[0].couterAll)
+  })
+  
   res.render('index', { title: 'Express' });
 });
 
